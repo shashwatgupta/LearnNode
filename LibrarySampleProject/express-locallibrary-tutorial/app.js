19340470377113d8467d84ff9c70075c632a56d3
Shashwat Gupta <shashwat.gupta@gmail.com>
2018-01-29 23:32:10 -0800

    Added Controller

--- LibrarySampleProject/express-locallibrary-tutorial/app.js
+++ LibrarySampleProject/express-locallibrary-tutorial/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog');
 
 var app = express();
 //Set up mongoose connection
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
